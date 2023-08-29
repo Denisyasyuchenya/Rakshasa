48e5bb5f0bfed19b976ea576b561d9f77763fa2f
Denis Yasyuchenya <yasyuchenya1@gmail.com>
2023-08-30 02:32:25 +0300

    feat: add pass generator

--- rakshasa/domain.ts
+++ rakshasa/domain.ts
@@ -25,3 +25,13 @@ export function generateRandomWord(): TE.TaskEither<Error, string> {
       (error) => new Error(String(error))
     );
   }
+
+  export function generatePassword() {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-";
+    let password = "";
+    for (let i = 0; i < 13; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    return password;
+  }
